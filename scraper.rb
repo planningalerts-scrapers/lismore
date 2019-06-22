9b02a79c9017786a301b7b047949720b007659db
Matthew Landauer <mlandauer@gmail.com>
2019-06-22 13:29:20 +1000

    Update to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,3 @@
 require "masterview_scraper"
 
-MasterviewScraper.scrape_and_save_period(
-  url: "http://tracker.lismore.nsw.gov.au",
-  period: :last30days,
-  use_api: true
-)
+MasterviewScraper.scrape_and_save(:lismore)
